30e5a9d58878e32a17cd9806cb8a11047f30714e
GabrielRegis <34111140+GabrielRegis@users.noreply.github.com>
2017-12-02 04:07:16 -0200

    ProjetoFinalUpdateVisual
    Foi atualizado o css e o html da lista de tarefas de modo a tornar o visual mais agradável

--- lista_tarefas/public/app.js
+++ lista_tarefas/public/app.js
@@ -1,3 +1,16 @@
 $(".tarefa").click(function(){
     $(this).toggleClass("feito")
 })
+
+/*$(".deletar").mouseenter(function(){
+    $(this).children().fadeIn(300,function(){
+        
+    });
+})
+
+$(".deletar").mouseleave(function(){
+    $(this).children().fadeOut(300,function(){
+        
+    });
+    
+})*/
